3ba8c1607c1ce6f205c249361d8570fd1a10e737
254mathenge <mathengefaith49@gmail.com>
2024-12-23 05:04:02 +0300

    map products

--- src/containers/ProductComponent.jsx
+++ src/containers/ProductComponent.jsx
@@ -1,26 +1,43 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import "./ProductComponent.css"
  const ProductComponent=() =>{
     const products=useSelector((state)=> state.allProducts.products);
-    const{id ,title,category}=products;
-    return(
+    if (!products || products.length === 0) {
+        return <div>No products available.</div>;
+    }
+    const renderList =products.map((product)=>{
+        const {id,image,title,price,category}=product;
+        return(
         
-        <div className="product-listing">
-             <div className="ui link ">
-             <div className="card">
-             <div className="image">
-                <img src="" alt/>
-             </div>
-             <div className="content">
-             <div className="header">{title}</div>
+            <div className="product-listing" key={product.id}>
+                <Link to={`/product/${id}`}>
+                 <div className="ui link ">
+                 <div className="card">
+                 <div className="image">
+                    <img src={image} alt={title}/>
+                 </div>
+                 <div className="content">
+                 <div className="header">{title}</div>
+                 <div className="meta">$ {price}</div>
+                 <div className="meta">{category}</div>
+                 
+                </div>
+                
             </div>
-        </div>
+                </div>
+                </Link>
             </div>
-        </div>
+        )
+    })
+    return(
+        <>{renderList}</>
     )
+    
  }
  export default ProductComponent;
--- src/containers/ProductListing.jsx
+++ src/containers/ProductListing.jsx
@@ -1,10 +1,24 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import { useSelector } from "react-redux";
+import React,{useEffect} from "react";
+import axios  from "axios";
+import { useSelector,useDispatch } from "react-redux";
+import {setProducts} from '../redux/actions/productActions';
 import ProductComponent from "./ProductComponent";
  const ProductListing =() =>{
     const products =useSelector((state) => state);
-    console.log(products);
+    const dispatch = useDispatch();
+    const fetchProducts =async () =>{
+        const response =await axios
+        .get("https://fakestoreapi.com/products")
+        .catch((err)=>{
+            console.log("Error fetching products:", err);
+         });
+      dispatch(setProducts(response.data));
+    };
+    useEffect(()=>{
+fetchProducts()
+    },[])
+    console.log("Products: ",products);
     return(
         
         <div className="product-listing">
